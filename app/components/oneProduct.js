1fa0ae885a77578e5eae7e29f17db56b6797a28f
spiffysparrow <tracykmullen@gmail.com>
2016-03-22 18:20:35 -0700

    setup list items

---
+++ app/components/oneProduct.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default React.createClass({
+  render() {
+    return (
+      <li className="Product">
+        <span>Products</span>
+      </li>
+    );
+  }
+});
--- app/components/products.js
+++ app/components/products.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import OneProduct from './oneProduct';
 
 export default React.createClass({
   render() {
+    var productList = Data.products.map(function(item, i){
+      return (<OneProduct key={i}/>)
+    });
     return (
       <div className="Products">
         <h2>Products</h2>
+        {productList}
       </div>
     );
   }
