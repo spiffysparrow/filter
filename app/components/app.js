e1dc5dd5d8afcc38c7ca6ce9604162515841668f
spiffysparrow <tracykmullen@gmail.com>
2016-03-30 17:05:35 -0700

    readme and title

--- app/components/app.js
+++ app/components/app.js
@@ -5,7 +5,7 @@ export default React.createClass({
   render() {
     return (
       <div className="App">
-        <h1>Huckberry</h1>
+        <h1>Meddlesome Fox</h1>
         <Products />
       </div>
     );
